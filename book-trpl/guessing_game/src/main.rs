5479306742db6190e77041cc781c2274400941a7
Cody Collier <cody@telnet.org>
2020-08-18 23:35:20 -0500

    Add secret number generation
    Bring Rng trait into scope,
    use thread_rng random number generator,
    and generate a rand number with gen_range()

--- book-trpl/guessing_game/src/main.rs
+++ book-trpl/guessing_game/src/main.rs
@@ -1,9 +1,16 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("");
     println!("Guess the number!");
     println!("");
+
+    // Generate the secret number
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println!("Ssshh! The secret number is: {}", secret);
+
+    // Accept a guess and process
     println!("Please enter your guess: ");
 
     let mut guess = String::new();
