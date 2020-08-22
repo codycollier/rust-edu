c38cfc61ce60cc638a4c4c360939f1ac5079daa7
Cody Collier <cody@telnet.org>
2020-08-22 14:38:58 -0500

    numeric literal forms

--- book-trpl/variables/src/main.rs
+++ book-trpl/variables/src/main.rs
@@ -20,5 +20,19 @@ fn main() {
     println!("x is: {}", x);
 
     // constant
-    println!("MAX_POINTS: {}", MAX_POINTS)
+    println!("MAX_POINTS: {}", MAX_POINTS);
+
+    // numeric literals
+    println!("number: {}", 255);
+    println!("number: {}", 0xff);
+    println!("number: {}", 0o377);
+    println!("number: {}", 0b1111_1111);
+    println!("number: {}", b'Z');
+    // println!("number: {}", b'■');
+    // println!("number: {}", b'ß');
+
+    println!("number: {}", 255u32);
+    println!("number: {}", 255i32);
+    println!("number: {}", 255usize);
+    println!("number: {}", 255isize);
 }
