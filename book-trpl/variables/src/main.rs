99a7554ff237b526dc5a1955c99e23397942fdb9
Cody Collier <cody@telnet.org>
2020-08-22 13:34:46 -0500

    variables and mutability

--- book-trpl/variables/src/main.rs
+++ book-trpl/variables/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("x is: {}", x);
+
+    let mut x = 6;
+    println!("x is: {}", x);
+
+    x = 7;
+    println!("x is: {}", x);
 }
