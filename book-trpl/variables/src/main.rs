4c240f315c9ea92e16812fa2de235e7c1f150cec
Cody Collier <cody@telnet.org>
2020-08-22 13:43:18 -0500

    shadowing etC

--- book-trpl/variables/src/main.rs
+++ book-trpl/variables/src/main.rs
@@ -4,11 +4,21 @@ fn main() {
     let x = 5;
     println!("x is: {}", x);
 
+    // not allowed. not mutable.
+    // x = 6;
+    // println!("x is: {}", x);
+
+    // shadow (inc type change)
+    let x = "six";
+    println!("x is: {}", x);
+
+    // shadow + mutable
     let mut x = 6;
     println!("x is: {}", x);
 
     x = 7;
     println!("x is: {}", x);
 
+    // constant
     println!("MAX_POINTS: {}", MAX_POINTS)
 }
