07e39130c9984cb497fc9878e2f2f77547e8b941
Cody Collier <cody@telnet.org>
2020-08-22 16:22:32 -0500

    compound data types

--- book-trpl/datatypes/src/main.rs
+++ book-trpl/datatypes/src/main.rs
@@ -4,20 +4,24 @@ fn main() {
     // constant
     println!("MAX_POINTS: {}", MAX_POINTS);
 
+    //-----------------------------------------------------------------------
+    // scalar types
+    //-----------------------------------------------------------------------
+
     // number / integer literals
-    println!("number: {}", 255);
-    println!("number: {}", 0xff);
-    println!("number: {}", 0o377);
-    println!("number: {}", 0b1111_1111);
-    println!("number: {}", b'Z');
-    // println!("number: {}", b'■');
-    // println!("number: {}", b'ß');
+    println!("integer: {}", 255);
+    println!("integer: {}", 0xff);
+    println!("integer: {}", 0o377);
+    println!("integer: {}", 0b1111_1111);
+    println!("integer: {}", b'Z');
+    // println!("integer: {}", b'■');
+    // println!("integer: {}", b'ß');
 
     // sample of some different forms
-    println!("number: {}", 255u32);
-    println!("number: {}", 255i32);
-    println!("number: {}", 255usize);
-    println!("number: {}", 255isize);
+    println!("integer: {}", 255u32);
+    println!("integer: {}", 255i32);
+    println!("integer: {}", 255usize);
+    println!("integer: {}", 255isize);
 
     // floats (32 single precision, 64 double precision)
     println!("float  : {}", 0.01234567890123456789);
@@ -46,4 +50,73 @@ fn main() {
 
     let remainder = 43 % 5;
     println!("remainder: {}", remainder);
+
+    // booleans
+    let mut foo = true;
+    println!("bool: {}", foo);
+
+    foo = false;
+    println!("bool: {}", foo);
+
+    let foo: bool = true;
+    println!("bool: {}", foo);
+
+    // char
+    // char literals in single quotes ''.  strings in "".
+    let bar = 'Z';
+    println!("char: {}", bar);
+
+    let bar = 'ℤ';
+    println!("char: {}", bar);
+
+    // char is 4 byte unicode scalar value
+    let heart_eyed_cat = '😻';
+    println!("char: {}", heart_eyed_cat);
+
+    //-----------------------------------------------------------------------
+    // primitive compound types
+    // tuples and arrays
+    // both are fixed length
+    // tuples allow mixed types
+    // arrays must contain elements all of same type
+    //-----------------------------------------------------------------------
+
+    // tuple
+    let tup: (i32, f32, &str) = (1, 2.3, "four");
+    println!("tup: {:?}", tup);
+
+    // destructuring a tuple, with pattern matching
+    let (a, b, c) = (1, 2.3, "four");
+    println!("tuple part 0: {}", tup.0);
+    println!("tuple part 0: {}", a);
+    println!("tuple part 1: {}", b);
+    println!("tuple part 2: {}", c);
+
+    // array (allocated on the stack)
+    let ar = [1, 2, 3, 4, 5];
+    println!("array: {:?}", ar);
+
+    let months: [&str; 12] = [
+        "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec",
+    ];
+    println!("months: {:?}", months);
+    println!("month1: {}", months[1]);
+
+    // zero'd array using alternate initializing
+    let ar = [0; 10];
+    println!("zero ar: {:?}", ar);
+
+    // panic time
+    // compile error:
+    // println!("show item 33: {}", ar[33]);
+    // compile error:
+    // let index = 33;
+    // println!("show item 33: {}", ar[index]);
+
+    // The book says this should be a run time panic, but
+    // it is caught at compile time
+    // let a = [1, 2, 3, 4, 5];
+    // let index = 33;
+    // let element = a[index];
+    // println!("show item 33: {}", element);
 }
