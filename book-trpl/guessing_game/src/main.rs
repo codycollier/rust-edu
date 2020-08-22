0debcf24fbb605970a3c505cf01a2b05d266fa6c
Cody Collier <cody@telnet.org>
2020-08-21 22:52:02 -0500

    comparison and matcher. loop.

--- book-trpl/guessing_game/src/main.rs
+++ book-trpl/guessing_game/src/main.rs
@@ -1,5 +1,11 @@
+// a trait
 use rand::Rng;
+
+// type
+use std::cmp::Ordering;
+
 use std::io;
+
 
 fn main() {
     println!("");
@@ -10,26 +16,30 @@ fn main() {
     let secret = rand::thread_rng().gen_range(1, 101);
     println!("Ssshh! The secret number is: {}", secret);
 
-    // Accept a guess and process
-    println!("Please enter your guess: ");
+    loop {
 
-    let mut guess_raw = String::new();
+        // Accept a guess and process
+        println!("Please enter your guess: ");
 
-    let guess_bytes = io::stdin()
-        .read_line(&mut guess_raw)
-        .expect("Failed to read line.");
+        let mut guess_raw = String::new();
 
-    let guess: i32 = guess_raw
-        .trim()
-        .parse()
-        .expect("Please enter numbers only.");
+        let guess_bytes = io::stdin()
+            .read_line(&mut guess_raw)
+            .expect("Failed to read line.");
 
-    println!("You guessed: {} (bytes: {})", guess, guess_bytes);
-    println!("");
+        let guess: i32 = guess_raw
+            .trim()
+            .parse()
+            .expect("Please enter numbers only.");
 
-    if guess == secret {
-        println!("You got it!");
-    } else {
-        println!("Incorrect, better luck next time!");
+        println!("You guessed: {} (bytes: {})", guess, guess_bytes);
+        println!("");
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too low."),
+            Ordering::Greater => println!("Too high."),
+            Ordering::Equal => println!("You guessed correctly!"),
+        }
+        println!("");
     }
 }
