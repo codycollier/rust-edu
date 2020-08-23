0a53b1ec67f7a24f6e06169a80b76c5aaaedef4b
Cody Collier <cody@telnet.org>
2020-08-22 19:41:51 -0500

    while loop countdown

--- book-trpl/loops/src/main.rs
+++ book-trpl/loops/src/main.rs
@@ -6,12 +6,13 @@ fn main() {
     let number = rng.gen_range(0, 11);
     println!("random number: {}", number);
 
+    // loop forever (until ctrl-c interrupt)
     //loop {
     //    println!("rust is cool!");
     //}
 
+    // loop with break and return val
     let mut counter = 0;
-
     let result = loop {
         if counter == number {
             break counter;
@@ -19,4 +20,12 @@ fn main() {
         counter += 1;
     };
     println!("      counter: {}", result);
+
+    // while loop
+    let mut countdown = 3;
+    while countdown != 0 {
+        println!("{}", countdown);
+        countdown -= 1;
+    }
+    println!("Lift off!");
 }
