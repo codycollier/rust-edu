12a70f79c58f5eb8b55bc00fd1237c1335728397
Cody Collier <cody@telnet.org>
2020-08-22 19:37:00 -0500

    for with break vals

--- book-trpl/loops/src/main.rs
+++ book-trpl/loops/src/main.rs
@@ -1,3 +1,22 @@
+use rand::thread_rng;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    let number = rng.gen_range(0, 11);
+    println!("random number: {}", number);
+
+    //loop {
+    //    println!("rust is cool!");
+    //}
+
+    let mut counter = 0;
+
+    let result = loop {
+        if counter == number {
+            break counter;
+        }
+        counter += 1;
+    };
+    println!("      counter: {}", result);
 }
