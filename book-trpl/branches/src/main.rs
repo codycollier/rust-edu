91c4f92df1e6b3989548d71d7edc6d9dbf195f2a
Cody Collier <cody@telnet.org>
2020-08-22 19:22:45 -0500

    if conditional. explore rand generation and choice.

--- book-trpl/branches/src/main.rs
+++ book-trpl/branches/src/main.rs
@@ -1,3 +1,33 @@
+use rand::seq::SliceRandom;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    // random gen from a range
+    let mut rng = rand::thread_rng();
+    let number = rng.gen_range(0, 10);
+    println!("number: {:?}", number);
+
+    if number < 5 {
+        println!("condition is true (< 5)");
+    } else {
+        println!("condition is false (>= 5)");
+    }
+
+    // random choice
+    // let number = numbers.choose(&mut rng).unwrap();
+    // if *number < 5 {
+    let numbers = [1, 3, 5, 7];
+    let mut rng = rand::thread_rng();
+    let &number = numbers.choose(&mut rng).unwrap();
+    println!("number: {:?}", number);
+
+    if number < 5 {
+        println!("condition is true (< 5)");
+    } else if number == 5 {
+        println!("condition is true (= 5)");
+    } else {
+        println!("conditions are false (> 5)");
+    }
+
+    //
 }
