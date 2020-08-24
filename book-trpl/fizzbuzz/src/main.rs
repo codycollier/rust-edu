78ad65deec638165876a51f630ca47d6db0fb71c
Cody Collier <cody@telnet.org>
2020-08-23 22:51:22 -0500

    practice

---
+++ book-trpl/fizzbuzz/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    for x in 1..101 {
+        if x % 15 == 0 {
+            println!("fizzbuzz");
+        } else if x % 3 == 0 {
+            println!("fizz");
+        } else if x % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", x)
+        }
+    }
+}
