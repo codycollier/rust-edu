73c45e9db895ad0ff79f244b32f15f56469fde09
Cody Collier <cody@telnet.org>
2020-08-25 00:22:11 -0500

    copy vs move

--- book-trpl/ownership/src/main.rs
+++ book-trpl/ownership/src/main.rs
@@ -23,4 +23,22 @@ fn main() {
     // s += ", world again!";
     s.push_str(", world again!");
     println!("s: {}", s);
+
+    //---------------------------------------
+    let x = 5;
+    // note: this is a copy
+    // (small, statically sized type on the stack)
+    let y = x;
+    // both are accessible
+    println!("x: {}, y: {}", x, y);
+
+    let a = String::from("boats");
+    // note: this is a move not a copy
+    // (heap/complex structure without a Copy trait
+    let b = a;
+    println!("b: {}", b);
+    // This won't work because a is no longer valid
+    // println!("a: {}", a)
+
+    //---------------------------------------
 }
