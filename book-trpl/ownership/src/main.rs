05707c0730d2bbde0cfb40bb92e026ae0c3daec3
Cody Collier <cody@telnet.org>
2020-08-24 23:40:45 -0500

    scope

--- book-trpl/ownership/src/main.rs
+++ book-trpl/ownership/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("outer scope!");
+
+    {
+        println!("inner scope!");
+        let s = "hello";
+        println!("s: {}", s)
+    }
+
+    println!("back to outer scope!");
+
+    // no longer in scope.  can't reference.
+    // println!("s: {}", s)
+
+    // new var and owner, back in scope
+    let s = "hi again";
+    println!("s: {}", s);
 }
