eed20bec4598a9baa9760d1fa4f82e92689719fe
Cody Collier <cody@telnet.org>
2020-08-27 23:55:06 -0500

    more notes on borrowing

--- book-trpl/ownership_references/src/main.rs
+++ book-trpl/ownership_references/src/main.rs
@@ -5,7 +5,15 @@ fn main() {
 }
 
 fn calculate_length(st: &String) -> usize {
-    // cannot assign to immutable argument
+    // cannot change the value at the reference or assign to the immutable arg
     // st = &String::from("foo");
+    // err: cannot assign to immutable argument
+
+    // said differently, due to the reference, st is merely borrowed not owned, so it can't be changed here
+
+    // cannot do this more direct change either
+    // st.push_str(", how are you");
+    // err: ^^ `st` is a `&` reference, so the data it refers to cannot be borrowed as mutable
+
     st.len()
 } // st reference goes out of scope, but with no ownership of value, which means no drop
