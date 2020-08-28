88ebfe2be40648813a736546411546e9859bcc9b
Cody Collier <cody@telnet.org>
2020-08-27 23:45:58 -0500

    pass reference, with mod not allowed, to retain ownership and let fn read

--- book-trpl/references/src/main.rs
+++ book-trpl/references/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello there");
+    let slen = calculate_length(&s);
+    println!("string: '{}' is len {}", s, slen)
+}
+
+fn calculate_length(st: &String) -> usize {
+    // cannot assign to immutable argument
+    // st = &String::from("foo");
+    st.len()
 }
