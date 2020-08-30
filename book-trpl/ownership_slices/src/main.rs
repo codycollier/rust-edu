f80abef578afa55b814f5d04d02c1666530a93c3
Cody Collier <cody@telnet.org>
2020-08-29 19:52:36 -0500

    more exploration of slices and borrowing

--- book-trpl/ownership_slices/src/main.rs
+++ book-trpl/ownership_slices/src/main.rs
@@ -1,11 +1,34 @@
 fn main() {
     let s = String::from("win the stage");
     // let s = String::from("stage");
-    let fw = first_word(&s);
-    println!("fw: {}", fw)
+
+    let efw = end_of_first_word(&s);
+    // this will clear s and efw is no longer correct
+    // s.clear();
+    println!("efw: {}", efw);
+
+    let fw1 = first_word(&s);
+    // now, by using a slice and true reference to s, we get an error
+    // s.clear();
+    println!("fw1: {}", fw1);
+
+    let fw2 = first_word_efw(&s);
+    println!("fw2: {}", fw2);
 }
 
 fn first_word(s: &String) -> &str {
+    let sbytes = s.as_bytes();
+    for (i, &item) in sbytes.iter().enumerate() {
+        if item == b' ' {
+            // return the index of the end of the word
+            return &s[..i];
+        }
+    }
+    // no space? return index of final spot
+    &s[..]
+}
+
+fn first_word_efw(s: &String) -> &str {
     let efw = end_of_first_word(s);
     &s[..efw]
 }
