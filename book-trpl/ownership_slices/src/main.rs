70c59fcf15f226a147249e4b2f3fc724c7d899fd
Cody Collier <cody@telnet.org>
2020-08-29 19:33:51 -0500

    exercises with index

--- book-trpl/ownership_slices/src/main.rs
+++ book-trpl/ownership_slices/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("win the stage");
+    let fw = end_of_first_word(&s);
+    println!("fw: {}", fw)
+}
+
+fn end_of_first_word(s: &String) -> usize {
+    let sbytes = s.as_bytes();
+    for (i, &item) in sbytes.iter().enumerate() {
+        if item == b' ' {
+            // return the index of the end of the word
+            return i;
+        }
+    }
+    // no space? return index of final spot
+    return s.len();
 }
