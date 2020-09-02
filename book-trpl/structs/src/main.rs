25ec3d04d403540e7612aad73151db8b628be8e9
Cody Collier <cody@telnet.org>
2020-09-01 20:22:37 -0500

    struct making

--- book-trpl/structs/src/main.rs
+++ book-trpl/structs/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug)]
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    };
+
+    let user = User {
+        username: String::from("beatrice"),
+        email: String::from("b@somewhere.com"),
+        sign_in_count: 1,
+        active: true,
+    };
+    println!("user: {:?}", user);
 }
